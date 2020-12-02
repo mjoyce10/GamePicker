2192004d3c9e917ede08c344f42c5d8bbac325ec
Maureen Joyce <mjoyce10@kent.edu>
2020-12-02 10:15:07 -0500

    added default user avatar

--- frontend-spa/src/js/main.js
+++ frontend-spa/src/js/main.js
@@ -83,7 +83,12 @@ function displayUserNickname() {
         const userName = result.response.players[0].personaname
         console.log(userName)
         userWelcomeElement.innerHTML = `Welcome, ${userName}`
-        userAvatarElement.setAttribute("src", userAvatar)
+        if(userAvatar === 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg') {
+            userAvatarElement.setAttribute("src", '../../images/default-avatar.png')
+        }
+        else {
+            userAvatarElement.setAttribute("src", userAvatar)
+        }
     })
 }
 

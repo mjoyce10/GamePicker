c7dd42604b972cedf4afbb399fbf2b5dcc9eb3e8
Hugh McGee <hugh41971@gmail.com>
2020-12-04 11:01:26 -0500

    renamed FriendsList to  FavoriteFriend, set up startup.cs

---
+++ backend/GamePicker/Repositories/FriendsListRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GamePicker.Models;
+
+namespace GamePicker.Repositories
+{
+    public class FriendsListRepository : Repository<FriendsList>, IRepository<FriendsList>
+    {
+        GPContext db;
+        public FriendsListRepository(GPContext context) : base(context)
+        {
+            db = context;
+        }
+    }
+}

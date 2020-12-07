4645919bab853b36b124ee727da79d22853223fc
Maureen Joyce <mjoyce10@kent.edu>
2020-12-07 13:53:55 -0500

    added fruit to sign in screen

--- frontend-spa/src/js/components/EnterSteamID.js
+++ frontend-spa/src/js/components/EnterSteamID.js
@@ -1,11 +1,15 @@
 export default function EnterSteamID() {
     return `
     <h2 class="enter-steam-id-title">Enter your Steam ID</h2>
+    <div class="images-and-input">
+    <img class="cherry fruit" src="../../../images/cherry.png"/>
     <div class="steam-id-container">
         <div class="inner-steam-id-container">
             <input class="steam-id-input" placeholder="Steam ID" type="text"/>
             <img class="steam-id-button" src="../../../images/quarter_slot.jpg"/>
         </div>
+    </div>
+    <img class="strawberry fruit" src="../../../images/strawberry.png"/>
     </div>
     <h2 class="recent-users-heading">Recent Players</h2>
     <div class="recent-users"></div>

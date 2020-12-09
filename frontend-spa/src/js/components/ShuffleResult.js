e9eb44852f6b2d62998167b0c7855f99dc47d798
Ragereous <ragereous@gmail.com>
2020-12-09 10:50:17 -0500

    styled shuffle button and game choice image, applied styling to all buttons

--- frontend-spa/src/js/components/ShuffleResult.js
+++ frontend-spa/src/js/components/ShuffleResult.js
@@ -1,8 +1,10 @@
 export default function ShuffleResult(){
     return `
     <h2>You should play:</h2>
+    <div class="shuffle-container">
     <p class="game-choice"></p>
     <img class="game-choice-image"/>
+    </div>
     <button class="shuffle-btn">Reshuffle!</button>
     <h2>Player Stats</h2>
     <div class="stats-container">
